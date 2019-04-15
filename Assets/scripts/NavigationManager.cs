917bafe538fc60ece929e80a8abe961c2bfdf831
Jose Victor <jotavictorsb@gmail.com>
2019-04-15 08:59:34 -0300

    Cena Main e NavigationManager

---
+++ Assets/scripts/NavigationManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigationManager : MonoBehaviour
+{
+    public void LoadScene(string sceneName) {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName); 
+    }
+}
