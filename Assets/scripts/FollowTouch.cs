4631041b715f095888c7ad08e07277bb0502b701
Jose Victor <jotavictorsb@gmail.com>
2019-04-29 09:01:57 -0300

    Começando FollowTouch

---
+++ Assets/scripts/FollowTouch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTouch : MonoBehaviour
+{
+    public GameObject particles;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            Vector3 aux = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 position = new Vector3(aux.x, aux.y, 0);
+            Instantiate(particles, position, Quaternion.identity); 
+        }
+    }
+}
